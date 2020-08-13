01c1b59686a9ca58b47bf8f57e986f6956265ba2
maulanairfanf <maulanairfanf@gmail.com>
2020-08-13 21:04:50 +0700

    add carousel

--- covid-19/src/views/Home.vue
+++ covid-19/src/views/Home.vue
@@ -1,17 +1,42 @@
 <template>
   <div class="home container">
-    <h1>HomePage</h1>
+    <v-carousel>
+    <v-carousel-item class="rounded-lg"
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
     <div v-for="country in countrys" :key="country.name">
       <h2>{{country.name}}</h2>
     </div>
   </div>
 </template>
 
+
+
 <script>
+
 export default {
   data() {
     return {
       countrys: [],
+      items: [
+          {
+            src: '../assets/logo.png',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+        ],
     };
   },
   created() {
