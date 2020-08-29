8be2c201b3bc2dc3acf5900954d25ed8f6273a4f
maulanairfanf <maulanairfanf@gmail.com>
2020-08-30 00:42:37 +0700

    add icon

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  pwa: {
-    name: 'CovInfo',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-  },
+  // pwa: {
+  //   name: 'CovInfo',
+  //   short_name:'CovInfo',
+  //   themeColor: '#4DBA87',
+  //   msTileColor: '#000000',
+  //   appleMobileWebAppCapable: 'yes',
+  //   appleMobileWebAppStatusBarStyle: 'black',
+  //   start_url : ".",
+  //   display : "standalone",
+     
+  // },
   "transpileDependencies": [
     "vuetify"
   ],
