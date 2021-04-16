eab4dbb3df30c03f6fe46f25c6a8f5fd550e76d0
maulanairfanf <maulanairfanf@gmail.com>
2021-04-16 22:07:25 +0700

    edit page prevent

--- src/views/Prevent.vue
+++ src/views/Prevent.vue
@@ -2,18 +2,28 @@
   <v-container class="Symptoms container mt-6">
     <div>
       <v-flex xs12 md12 lg12 class="d-flex justify-center align-center">
-        <span class="heading-1 text-center grey--text text--darken-2 "> <span class="light-green--text text--darken-1">Protect</span> {{heading}}</span>
+        <span class="heading-1 text-center grey--text text--darken-2 ">
+          <span class="light-green--text text--darken-1">Protect</span>
+          {{ heading }}</span
+        >
       </v-flex>
-      <v-flex xs12 md12 lg12 class="d-flex justify-center align-center mt-2 mb-8">
+      <v-flex
+        xs12
+        md12
+        lg12
+        class="d-flex justify-center align-center mt-2 mb-8"
+      >
         <v-flex lg4>
-          <p class="sub-heading text-center grey--text text--darken-1">{{subHeading}}</p>
+          <p class="sub-heading text-center grey--text text--darken-1">
+            {{ subHeading }}
+          </p>
         </v-flex>
       </v-flex>
     </div>
 
     <v-row class="d-flex flex-wrap container-costum justify-center">
       <v-card
-        v-for="(pict,p) in picts"
+        v-for="(pict, p) in picts"
         :key="p"
         class="mx-auto mb-6 container justify-center align-center"
         flat
@@ -22,12 +32,12 @@
         max-width="450"
       >
         <v-flex class="d-flex justify-center">
-          <v-img max-width="250" :src="pict.src" contain></v-img>
+          <v-img max-width="250" height="200" :src="pict.src" contain></v-img>
         </v-flex>
 
         <v-card-text class="text-center">
-          <h2 class="mb-3 mt-6">{{pict.heading}}</h2>
-          <span class="sub-heading">{{pict.title}}</span>
+          <h2 class="mb-3 mt-6">{{ pict.heading }}</h2>
+          <span class="sub-heading">{{ pict.title }}</span>
         </v-card-text>
       </v-card>
     </v-row>
@@ -43,36 +53,36 @@ export default {
         "Sampai saat ini belum ditemukan vaksin yang dapat menyembuhkan virus corona, cara terbaik untuk saat ini adalah dengan cara menghindari virus tersebut. Berikut hal - hal yang perlu diperhatikan untuk menjaga diri dan orang di sekitar anda ",
       picts: [
         {
-          src: require("../assets/cuci-tangan.png"),
+          src: require("../assets/cuci-tangan.jpg"),
           heading: "Mencuci Tangan",
           title:
             "Harus rajin mencuci tangan dengan air mengalir menggunakan sabun atau dapat menggunakan handsanitizer",
         },
         {
-          src: require("../assets/tetap-dirumah.png"),
+          src: require("../assets/tetap-dirumah.jpg"),
           heading: "Dirumah",
           title: "Tetap dirumah melakukan segala aktivitas di dalam rumah",
         },
         {
-          src: require("../assets/masker.png"),
+          src: require("../assets/masker.jpg"),
           heading: "Masker",
           title:
             "Gunakan masker jika terpaksa keluar, dan cuci masker secara berkala",
         },
         {
-          src: require("../assets/bersihkan.png"),
+          src: require("../assets/bersihkan.jpg"),
           heading: "Kebersihan",
           title:
             "Jaga kebersihan barang yang sering anda pakai, lakukan pembersihan secara berkala",
         },
         {
-          src: require("../assets/tutup-mulut.png"),
+          src: require("../assets/tutup-mulut.jpg"),
           heading: "Menutup Mulut",
           title:
             "Jika ingin bersin atau batuk tutupi mulut anda dengan siku atau tisu",
         },
         {
-          src: require("../assets/hidup-sehat.png"),
+          src: require("../assets/hidup-sehat.jpg"),
           heading: "Pola Sehat",
           title:
             "Jaga kesehatan dengan konsumsi makanan bergizi dan rajin berolahraga",
